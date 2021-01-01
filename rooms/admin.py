7fb83cc66f68143db990aa2ee6db6183deb23221
b-chae <cbr5964@naver.com>
2021-01-01 15:15:38 +0900

    Finish up Photo Admin
    Signed-off-by: b-chae <cbr5964@naver.com>

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
 from . import models
 
 
@@ -62,4 +63,12 @@ class RoomAdmin(admin.ModelAdmin):
 
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
-    pass
+
+    """ Photo Admin Definition """
+
+    list_display = ('__str__', 'get_thumbnail')
+
+    def get_thumbnail(self, obj):
+        return mark_safe(f'<img height="50px" src="{obj.file.url}" />')
+
+    get_thumbnail.short_description = "Thumbnail"
