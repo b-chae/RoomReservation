452c4657ab7d984758a90761c63dcb28071bcea3
b-chae <cbr5964@naver.com>
2021-01-05 15:31:07 +0900

    HomeView using Django Class Based View
    Signed-off-by: b-chae <cbr5964@naver.com>

--- rooms/views.py
+++ rooms/views.py
@@ -1,25 +1,21 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.core.paginator import Paginator
+from django.utils import timezone
+from django.views.generic import ListView
 from .import models
 
+# Limiting Querysets
+# all_rooms = models.Room.objects.all()[:5]
+# just pointing the DB, not loading
 
-def all_rooms(request):
-    # Limiting Querysets
-    # all_rooms = models.Room.objects.all()[:5]
-    # just pointing the DB, not loading
+# url에서 오는 모든 것은 Get Request 이다 !
+# print(request.GET)
+# http://127.0.0.1:8000/?page=1&city=seoul
 
-    # url에서 오는 모든 것은 Get Request 이다 !
-    # print(request.GET)
-    # http://127.0.0.1:8000/?page=1&city=seoul
 
-    page = int(request.GET.get("page", 1) or 1)
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10, orphans=3)
-    rooms = paginator.get_page(page)
-    print(vars(rooms.paginator))
+class HomeView(ListView):
+    """ HomeView Definition """
 
-    return render(request, "rooms/home.html",
-                  context={
-                      "pages": rooms,
-                  })
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    page_kwarg = "page"
